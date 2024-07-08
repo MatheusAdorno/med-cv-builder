21a291e5af066d01f8457e7fc5469361717f932e
Matheus Adorno <matheusadorno11@gmail.com>
2024-07-07 22:01:50 -0300

    changing a anchor to link

--- src/home/index.jsx
+++ src/home/index.jsx
@@ -1,5 +1,6 @@
 import Header from '@/components/custom/Header'
 import { ChevronDown, UserIcon, FileText, Download } from 'lucide-react'
+import { Link } from 'react-router-dom'
 
 function Home() {
   return (
@@ -16,10 +17,10 @@ function Home() {
               Build Your Medical Curriculum <span className='text-primary'>With AI</span> </h1>
             <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Effortlessly create a standout curriculum vitae for your clerkship applications with our AI-powered builder.</p>
             <div className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
-              <a href="/auth/sign-in" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-primary hover:bg-primary focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
+            <Link to={'/auth/sign-in'} className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-primary hover:bg-primary focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
                 Get Started
                 <svg className="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
-              </a>
+              </Link>
               {/* <a href="https://youtu.be/Q5LM985yUmQ" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
                 <svg className="mr-2 -ml-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"></path></svg>
                 Watch video
@@ -35,11 +36,11 @@ function Home() {
           <h2 className="text-md text-gray-500">Create your medical CV in just 3 simple steps</h2>
 
           <div className="mt-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
-            <a
+          <Link to={'/auth/sign-in'}
               className="block rounded-xl border bg-white
                 border-gray-200 p-8 shadow-xl transition
                 hover:border-primary/10 hover:shadow-primary/10"
-              href="/auth/sign-in"
+              
             >
               <UserIcon className='h-8 w-8' />
 
@@ -48,11 +49,10 @@ function Home() {
               <p className="mt-1 text-sm text-gray-600">
                 Provide your personal, educational, and work experience details to get started.
               </p>
-            </a>
+            </Link>
 
-            <a
+            <Link to={'/auth/sign-in'}
               className="block rounded-xl border bg-white border-gray-200 p-8 shadow-xl transition hover:border-primary/10 hover:shadow-primary/10"
-              href="/auth/sign-in"
             >
               <FileText className='h-8 w-8' />
 
@@ -61,11 +61,10 @@ function Home() {
               <p className="mt-1 text-sm text-gray-600">
                 Customize your CV with our easy-to-use editor, powered by AI suggestions.
               </p>
-            </a>
+            </Link>
 
-            <a
+            <Link to={'/auth/sign-in'}
               className="block rounded-xl border bg-white border-gray-200 p-8 shadow-xl transition hover:border-primary/10 hover:shadow-primary/10"
-              href="/auth/sign-in"
             >
               <Download className='h-8 w-8' />
 
@@ -74,13 +73,13 @@ function Home() {
               <p className="mt-1 text-sm text-gray-600">
                 Download your CV and share it with potential clerkship opportunities and employers.
               </p>
-            </a>
+            </Link>
           </div>
 
           <div className="mt-12 text-center">
-            <a href="/auth/sign-in" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-primary hover:bg-primary focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
+          <Link to={'/auth/sign-in'} className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-primary hover:bg-primary focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
                 Get Started Today
-              </a>
+              </Link>
           </div>
         </section>
       </div>
