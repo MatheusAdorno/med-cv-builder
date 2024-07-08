b467ab1bd40df8a78f55078fa0830fec05b534a1
Matheus Adorno <matheusadorno11@gmail.com>
2024-07-08 07:40:07 -0300

    changes for fix url

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,23 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      // eslint-disable-next-line no-undef
-      "@": path.resolve(__dirname, "./src"),
+  server: {
+    open: true,
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom'],
+        },
+      },
     },
   },
-})
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+});
